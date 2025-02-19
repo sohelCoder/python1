b2ece2094b0a62e34416dedfe620fb7bd2e76769
sohelCoder <sohelmujwar2005@gmail.com>
2025-02-19 20:44:18 +0530

    list

---
+++ 2.list.py
@@ -0,0 +1,22 @@
+My_Name=["sohel","vishal","chetan","sagar"]
+print(My_Name)
+My_Name[::-1]
+print(My_Name[::-1])
+My_Name.remove("sagar")
+print(My_Name)
+My_Name.insert(2,"rajesha")
+print(My_Name)
+My_Name.append("sandeep")
+print(My_Name)
+My_Name[-1]
+print(My_Name[-1])
+print(len (My_Name))
+print(max(My_Name))
+print(min(My_Name))
+print("sohel" in (My_Name))
+print("vishal" in (My_Name))
+print("chetan" in (My_Name))
+print("rajesha" in (My_Name))
+print("sandeep" in (My_Name))
+My_Name[4]="sahil"
+print(My_Name)
